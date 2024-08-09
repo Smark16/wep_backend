b563078b6f2f95d7deae44923b20a2a8cbbb08fc
Smark16 <sengendomark16@gmail.com>
2024-08-09 10:10:22 +0300

    merge

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l21#p-9irikflzbgw&9%a8i^v7rg3g_v4j@@jnw78uqxh2ztc)'
+#SECRET_KEY = 'django-insecure-l21#p-9irikflzbgw&9%a8i^v7rg3g_v4j@@jnw78uqxh2ztc)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
