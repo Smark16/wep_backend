3a820b895877c4591dd154532d5b6ff97b9f02c3
Smark16 <sengendomark16@gmail.com>
2024-08-09 10:28:50 +0300

    merge

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
