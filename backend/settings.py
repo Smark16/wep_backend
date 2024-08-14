cd6b28dfcfc8e6be1b8f164e9c39c39a2b416b12
Smark16 <sengendomark16@gmail.com>
2024-08-14 17:13:36 +0300

    meerge

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
    "http://localhost:5173",
-   "https://wepform.onrender.com"
+   "https://wepform.onrender.com",
+   "https://referal-frontend.onrender.com",
 ]
 
 REST_FRAMEWORK = {
